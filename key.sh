1c6f28ba25a03d005e8dd234d9eeff48ea8591e5
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-01-09 14:17:25 +0100

    fix

--- key.sh
+++ key.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 CONFIG_FILE="/root/.firo/firo.conf"
-sed -i "s/^znodeblsprivkey=$/znodeblsprivkey=$1/" $CONFIG_FILE
-echo -e "New znodeblsprivkey created - $1"
+KEY_PLACEHOLDER="znodeblsprivkey"
+sed -i "/^$KEY_PLACEHOLDER/d" "$CONFIG_FILE"
+echo "$KEY_PLACEHOLDER=$1" >> $CONFIG_FILE
+echo -e "[NEW] ${KEY_PLACEHOLDER} created - $1"
 echo -e ""
