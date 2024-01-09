368014b0caccb3617d685c3a7da6b298112eb390
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2024-01-09 12:36:29 +0100

    update config create

--- node_initialize.sh
+++ node_initialize.sh
@@ -1,37 +1,38 @@
 #!/usr/bin/env bash
-
+CONFIG_FILE="/root/.firo/firo.conf"
 function get_ip() {
     WANIP=$(curl --silent -m 15 https://api4.my-ip.io/ip | tr -dc '[:alnum:].')
     if [[ "$WANIP" == "" || "$WANIP" = *htmlhead* ]]; then
-        WANIP=$(curl --silent -m 15 https://checkip.amazonaws.com | tr -dc '[:alnum:].')    
-    fi  
+        WANIP=$(curl --silent -m 15 https://checkip.amazonaws.com | tr -dc '[:alnum:].')
+    fi
     if [[ "$WANIP" == "" || "$WANIP" = *htmlhead* ]]; then
         WANIP=$(curl --silent -m 15 https://api.ipify.org | tr -dc '[:alnum:].')
     fi
 }
 
-get_ip
-RPCUSER=$(pwgen -1 8 -n)
-PASSWORD=$(pwgen -1 20 -n)
-
-if [[ -f /root/.firo/firo.conf ]]; then
-  rm  /root/.firo/firo.conf
+if [[ ! -f $CONFIG_FILE ]]; then
+  get_ip
+  RPCUSER=$(pwgen -1 8 -n)
+  PASSWORD=$(pwgen -1 20 -n)
+  # Append or create lines in the configuration file
+  echo "rpcuser=$RPCUSER" >> $CONFIG_FILE
+  echo "rpcpassword=$PASSWORD" >> $CONFIG_FILE
+  echo "rpcallowip=127.0.0.1" >> $CONFIG_FILE
+  echo "listen=1" >> $CONFIG_FILE
+  echo "server=1" >> $CONFIG_FILE
+  echo "daemon=1" >> $CONFIG_FILE
+  echo "logtimestamps=1" >> $CONFIG_FILE
+  echo "txindex=1" >> $CONFIG_FILE
+  echo "znode=1" >> $CONFIG_FILE
+  echo "externalip=$WANIP:8168" >> $CONFIG_FILE
+  echo "znodeblsprivkey=$KEY" >> $CONFIG_FILE
 fi
 
-touch /root/.firo/firo.conf
-cat << EOF > /root/.firo/firo.conf
-rpcuser=$RPCUSER
-rpcpassword=$PASSWORD
-rpcallowip=127.0.0.1
-listen=1
-server=1
-daemon=1
-logtimestamps=1
-txindex=1
-znode=1
-externalip=$WANIP:8168
-znodeblsprivkey=$KEY
-EOF
+# Check if znodeblsprivkey is empty or not
+if grep -q "^znodeblsprivkey=$" $CONFIG_FILE; then
+    # Replace with the provided key
+    sed -i "s/^znodeblsprivkey=$/znodeblsprivkey=$KEY/" $CONFIG_FILE
+fi
 
 while true; do
  if [[ $(pgrep firod) == "" ]]; then 
